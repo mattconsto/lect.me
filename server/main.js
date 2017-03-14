f04d79a04e749e32736f4609053983dcbf3d8d9d
Matthew Consterdine <mrconsterdine@googlemail.com>
2017-03-14 19:27:46 +0000

    Fix sidebar and indentation

--- server/main.js
+++ server/main.js
@@ -3,5 +3,5 @@ import { Meteor } from 'meteor/meteor';
 import '../imports/api/messages.js';
 
 Meteor.startup(() => {
-  // code to run on server at startup
+	// code to run on server at startup
 });
