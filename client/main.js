32fd1d89bf6c8ee5c5bc9918e6f0d3af717e60e2
Matthew Consterdine <mrconsterdine@googlemail.com>
2017-03-18 18:16:38 +0000

    Add iframe sandboxing, and iframe protection

--- client/main.js
+++ client/main.js
@@ -13,3 +13,17 @@ Template.registerHelper('concat', function() {
 Template.registerHelper('qr', function(text) {
 	return require("qr-image").imageSync(text, {ec_level: 'L', type: 'svg'});
 });
+
+const iframe = function () {
+	try {
+		return window.self !== window.top;
+	} catch (e) {
+		return true;
+	}
+};
+
+Template.registerHelper('iframe', function () {
+	return iframe();
+});
+
+if(iframe()) top.location = self.location.href;
