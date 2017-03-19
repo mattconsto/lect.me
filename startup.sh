740b174bd31838977916f59fb1f67f25e0e9b582
Matthew Consterdine <mrconsterdine@googlemail.com>
2017-03-19 19:57:19 +0000

    Fix locale issue

--- startup.sh
+++ startup.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
-locale-gen en_US en_US.UTF-8
+# Configure timezone and locale
+# http://serverfault.com/a/689947
+echo "Europe/London" > /etc/timezone && \
+  dpkg-reconfigure -f noninteractive tzdata && \
+  sed -i -e 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen && \
+  sed -i -e 's/# en_GB.UTF-8 UTF-8/en_GB.UTF-8 UTF-8/' /etc/locale.gen && \
+  echo 'LANG="en_GB.UTF-8"'> /etc/default/locale && \
+  dpkg-reconfigure --frontend=noninteractive locales && \
+  update-locale LANG=en_GB.UTF-8
+
+# Start Nginx proxy
 nginx
+
+# Start our app
 cd /project/meteor
 meteor npm update
 meteor --allow-superuser
+
+# Wait for ever, so docker doesn't kill this VM
 bash
